4d0b9b210d909419c15674da5559b50da1a33792
Torobaev23 <daniyartorobaev23@gmail.com>
2023-12-20 01:21:31 +0600

    homework5

---
+++ homework5/app.js
@@ -0,0 +1,19 @@
+// // ДЗ №1
+// const list = [2,3,4,5,44,44,34,2,3,4,5,6,7,7,6,5,4,3,8,7,8,7,5,6,4,5]
+// const result = list.filter((current, listener , value) => {
+//     return  value.indexOf(current) === listener
+// })
+// console.log(result)
+//
+// // ДЗ №2
+// function getAll (...name) {
+//     const books = name.filter((page) => page.includes('у'))
+//     const books2 = name.filter((page) => !page.includes('у'))
+//     const result2 = books.map((desc) =>  `${desc} - 'книга с буквой «у»`)
+//     console.log(result2)
+//     console.log(books2)
+// }
+//
+// getAll(  'азбука' ,'плаха', 'первый учитель', 'унесенные ветрами' , 'ак-кеме')
+
+
