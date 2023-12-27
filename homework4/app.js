fe51cb788e2c1d9641cb9c55b1388a7b39d901f0
Torobaev23 <daniyartorobaev23@gmail.com>
2023-12-27 15:00:58 +0600

    kkk

--- homework4/app.js
+++ homework4/app.js
@@ -4,27 +4,34 @@ var minus = document.querySelector('#btn-2')
 var delete1 = document.querySelector('#delete')
 
 
-    plus.addEventListener('click' ,  () =>  {
-           var color1 =  input.value++
-        if (color1 => 0 ) {
+plus.addEventListener('click' ,  () =>  {
+        var color1 =  input.value++
+        if (color1 === 0) {
             input.style.backgroundColor = 'green'
-         }
-    }
-     )
-    minus.addEventListener('click' ,  () => {
-         var color2 = input.value--
-        if (color2 <= 0) {
-            input.style.backgroundColor = 'red'
+        } if (input.value === '0') {
+            input.style.backgroundColor = 'gray'
         }
-    }
-     )
-    delete1.addEventListener('click' , () => {
+}
+)
+
+
+minus.addEventListener('click' ,  () => {
+        var color2 = input.value--
+        if (color2 === 0) {
+            input.style.backgroundColor = 'red'
+        } if (input.value === '0'){
+            input.style.backgroundColor = 'gray'
+        }
+}
+)
+delete1.addEventListener('click' , () => {
         var color3 = input.value = '0'
         if (color3 === '0') {
             input.style.backgroundColor = 'gray'
         }
     }
-     )
+)
+
 
 
 
